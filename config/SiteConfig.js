e0740ebf3b96763dfb92245dae99cd8816ee6040
Mark Smithson <mark@smithson.me>
2020-10-04 14:28:20 +0100

    change banner url

--- config/SiteConfig.js
+++ config/SiteConfig.js
@@ -5,7 +5,7 @@ module.exports = {
   siteTitleAlt: "Mark's Musings - Thoughts on product, software, business and other random things", // Alternative Site title for SEO
   siteUrl: 'https://mark.smithson.me', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
-  siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
+  siteBanner: '/social/banner-marks-musings.jpg', // Your image for og:image tag. You can find it in the /static folder
   favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
   siteDescription: 'Thoughts on product, software, business and other random things', // Your site description
   author: 'MarkSmithson', // Author for schemaORGJSONLD
